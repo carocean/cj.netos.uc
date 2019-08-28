1fe24c3edf4874d008575816c5421a94fa42cdae
carocean <carocean.jofers@outlook.com>
2019-08-28 21:04:51 +0800

    1.0.0 于马尼拉

--- cj.netos.uc.openport/src/cj/netos/uc/service/ITenantTokenService.java
+++
@@ -1,5 +0,0 @@
-package cj.netos.uc.service;
-
-public interface ITenantTokenService {
-	String genToken(String tenant,String user,long ttlMillis);
-}
--- cj.netos.uc.openport/src/cj/netos/uc/service/ITokenService.java
+++
@@ -1,6 +0,0 @@
-package cj.netos.uc.service;
-
-public interface ITokenService {
-	boolean verify(String token);
-	String parseToken(String token);
-}
