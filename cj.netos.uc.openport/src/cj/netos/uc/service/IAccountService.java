a259a79af7fb80976b9cba453b02ab66187a1456
carocean <carocean.jofers@outlook.com>
2019-09-16 21:41:57 +0800

    1.0.0

---
+++ cj.netos.uc.openport/src/cj/netos/uc/service/IAccountService.java
@@ -0,0 +1,5 @@
+package cj.netos.uc.service;
+
+public interface IAccountService {
+
+}
---
+++ cj.netos.uc.openport/src/cj/netos/uc/service/ITenantService.java
@@ -0,0 +1,14 @@
+package cj.netos.uc.service;
+
+import cj.netos.uc.domain.UcTenant;
+import cj.studio.ecm.net.CircuitException;
+
+import java.util.List;
+
+public interface ITenantService {
+    String addTenant(String name,String website,String creator,String secret_key)throws CircuitException;
+    void removeTenant(String tenantid)throws CircuitException;
+    List<UcTenant> pageTenant(int currPage, int pageSize)throws CircuitException;
+    UcTenant getTenantById(String tenantid)throws CircuitException;
+    UcTenant getTenantByUser(String uid)throws CircuitException;
+}
